02bdb716da9ec7af990a6db786b82b681ab29b73
Daniel Sidabutar <aguspermadi94@gmail.com>
2017-09-09 13:47:35 +0700

    Initial core folder using redux for management state

---
+++ CoffeeGram/src/Components/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import {
+  View,
+  Text,
+} from 'react-native';
+
+class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render () {
+    return (
+      <View>
+        <Text>Home Screen!</Text>
+      </View>
+    )
+  }
+}
+
+export default HomeScreen;
